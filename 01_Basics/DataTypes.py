3ce98ac5d711cddde6201df59d1104c04787de6e
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-02 23:58:12 +0530

    Organize 01_Basics folder

---
+++ 01_Basics/DataTypes.py
@@ -0,0 +1,21 @@
+def main():
+
+    Name = "Dnyaneshwar Shinde"
+    No = 11
+    Value = 90.99
+    Flag = True
+
+    print("String : ", Name)
+    print("Type : ",type(Name))
+
+    print("Integer : ", No)
+    print("Type : ",type(No))
+
+    print("Float : ", Value)
+    print("Type : ",type(Value))
+
+    print("Boolean : ", Flag)
+    print("Type : ",type(Flag))
+
+if __name__ == "__main__":
+    main()
---
+++ 01_Basics/InputOutput.py
@@ -0,0 +1,10 @@
+def main():
+    No = 0
+
+    print("Enter number : ")
+    No = int(input())
+
+    print("Entered number is : ",No)
+
+if __name__ == "__main__":
+    main()
