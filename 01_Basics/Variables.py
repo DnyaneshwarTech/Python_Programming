bdb05251652dfad50bafab3d60334de3427b9d8b
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-03 11:38:57 +0530

    Add variables program in Python

---
+++ 01_Basics/Variables.py
@@ -0,0 +1,33 @@
+def main():
+
+    # Variables in python
+
+    grade = 'A'
+    name = "Dnyaneshwar"
+    age = 25
+    marks = 99.89
+    salary = 78.67890
+    is_student = True
+
+    # Print Values
+
+    print("Grade : ", grade)
+    print("Name : ", name)
+    print("Age : ", age)
+    print("Marks : ", marks)
+    print("Salary : ", salary)
+    print("Student : ", is_student)
+
+    print()
+    
+    # Check Data Types
+
+    print(type(grade))
+    print(type(name))
+    print(type(age))
+    print(type(marks))
+    print(type(salary))
+    print(type(is_student))  
+
+if __name__ == "__main__":
+    main()
