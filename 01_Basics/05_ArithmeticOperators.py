5bf369e97fb5affcaf6553a00223ca8ee7092aeb
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-06 00:06:23 +0530

    Rename python basics files with proper sequence

---
+++ 01_Basics/05_ArithmeticOperators.py
@@ -0,0 +1,12 @@
+def main():
+    num1 = 20
+    num2 = 10
+
+    print("Addition = ", num1 + num2)
+    print("Subtraction = ", num1 - num2)
+    print("Multiplication = ", num1 * num2)
+    print("Division = ", num1 / num2)
+    print("Modulus = ", num1 % num2)
+
+if __name__ == "__main__":
+    main()
