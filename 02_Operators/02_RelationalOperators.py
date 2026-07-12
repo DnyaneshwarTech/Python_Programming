c7b4a2b69b4c928833dcedc7c58390c42d534069
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-12 23:51:54 +0530

    Add relational operators program

---
+++ 02_Operators/02_RelationalOperators.py
@@ -0,0 +1,19 @@
+def main():
+    No1 = 0
+    No2 = 0
+
+    print("Enter the first number : ")
+    No1 = int(input())
+
+    print("Enter the second number : ")
+    No2 = int(input())
+
+    print("\n No1 == No2 : ",No1 == No2)
+    print("No1 != No2 : ", No1 != No2)
+    print("No1 > No2 : ",No1 > No2)
+    print("No1 < No2 : ",No1 < No2)
+    print("No1 >= No2 : ",No1 >= No2)
+    print("No1 <= No2 : ",No1 <= No2)
+
+if __name__ == "__main__":
+    main()
