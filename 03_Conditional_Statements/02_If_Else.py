a20afd51926d0b4910febeae1ed9d77c96a5aedc
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-07-20 00:50:51 +0530

    Add Python program for if-else statement

---
+++ 03_Conditional_Statements/02_If_Else.py
@@ -0,0 +1,12 @@
+def main():
+    Marks = 0
+
+    Marks = int(input("Enter your marks : "))
+
+    if(Marks >= 35):
+        print("Congratulations! You are Pass.")
+    else:
+        print("Sorry! You are Fail.")
+
+if __name__ == "__main__":
+    main()
