c89251d6e01cc6ad886fefb1735337d27d826eaf
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-08 23:20:51 +0530

    Add Sum of Even Numbers program

---
+++ 04_For_Loops/07_Sum_1_to_10.py
@@ -0,0 +1,11 @@
+def main():
+    iSum = 0
+    iCnt = 0
+
+    for iCnt in range(1,11):
+        iSum = iSum + iCnt
+
+    print("Sum of numbers from 1 to 10 is : ", iSum)
+
+if __name__ == "__main__":
+    main()
