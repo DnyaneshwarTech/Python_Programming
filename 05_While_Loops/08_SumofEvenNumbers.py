b3f4c835973a2937b6512e50b8a00efbba176025
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-27 21:35:49 +0530

    Added sum of even numbers using while loop

---
+++ 05_While_Loops/08_SumofEvenNumbers.py
@@ -0,0 +1,14 @@
+def main():
+    iCnt = 1
+    iSum = 0
+
+    while iCnt <= 10:
+        if iCnt % 2 == 0:
+            iSum = iSum + iCnt
+
+        iCnt = iCnt + 1
+
+    print(iSum)
+
+if __name__ == "__main__":
+    main()
