1e056222b73d5b8ca0879ebd90a65ddf1bf1da50
Dnyaneshwar Shinde <dnyaneshwarsshinde1@gmail.com>
2026-08-27 21:26:43 +0530

    Added table program using while loop

---
+++ 05_While_Loops/06_Table.py
@@ -0,0 +1,11 @@
+def main():
+    iNum = int(input("Enter number : "))
+
+    iCnt = 1
+
+    while iCnt <= 10:
+        print(iCnt * iNum)
+        iCnt = iCnt + 1
+
+if __name__ == "__main__":
+    main()
